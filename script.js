2ac6c049455fc6856de60c1ebed6a79f3322a847
Edgar Karapetyan <edo099919956@gmail.com>
2023-01-28 16:33:09 +0400

    opdate

--- script.js
+++ script.js
@@ -18,8 +18,19 @@ document.querySelector('.nav-right-corner-btn-open').addEventListener('click', (
         document.querySelector('.nav-right-corner').style.right = '-37px'
         document.querySelector('.nav-right-corner-btn-open .arrow').style.cssText = 'transform: rotate(0deg);'
     } else {
-        document.querySelector('.nav-right-corner').style.right ='0px'
+        document.querySelector('.nav-right-corner').style.right = '0px'
         document.querySelector('.nav-right-corner-btn-open .arrow').style.cssText = 'transform: rotate(180deg);'
     }
 
+})
+// mobile panel open close
+const nav_items_cont_mob = document.querySelector('.nav-items-cont-mob')
+document.querySelector('.nav-items-cont-mob-close').addEventListener('click', () => {
+    nav_items_cont_mob.style.cssText = 'opacity: 0.5; top: -80vh;'
+})
+document.querySelector('.nav-items-cont-mob-open').addEventListener('click', () => {
+    nav_items_cont_mob.style.cssText = 'opacity: 1; top: 0;'
+})
+window.addEventListener('scroll', () => {
+    nav_items_cont_mob.style.cssText = 'opacity: 0.5; top: -80vh;'
 })
